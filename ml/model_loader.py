1c31958354d78a2acbe3fd9b2b5c5a3bfb9b027f
ssimidev <111663923+ssimidev@users.noreply.github.com>
2025-11-19 20:43:00 -0300

    Update model_loader.py

--- ml/model_loader.py
+++ ml/model_loader.py
@@ -1,10 +1,15 @@
 import pandas as pd
 import numpy as np
 import joblib
+import shap
+
 
 model = joblib.load("ml/model_rf.pkl")
 preprocess = joblib.load("ml/preprocess.pkl")
-explainer = joblib.load("ml/shap_explainer.pkl")
+
+
+explainer = shap.TreeExplainer(model)
+
 
 EXPECTED_COLUMNS = [
     "Person ID",
@@ -22,27 +27,26 @@ EXPECTED_COLUMNS = [
 ]
 
 def predict(data: dict):
-    # Asegurar orden y columnas faltantes
+
     row = {col: data.get(col, None) for col in EXPECTED_COLUMNS}
     df = pd.DataFrame([row])
 
-    # Transformación del pipeline
-    X = preprocess.transform(df)
 
+    X = preprocess.transform(df)
 
     if hasattr(X, "toarray"):
         X = X.toarray()
 
-    # Convertir SIEMPRE a float64
     X = X.astype(np.float64)
 
-    # Predicción
+   
     y_pred = model.predict(X)[0]
 
-    # SHAP seguro
-    shap_vals = explainer.shap_values(X)[0].tolist()
+
+    shap_values = explainer.shap_values(X)
+    shap_values_list = shap_values[0].tolist()
 
     return {
         "prediction": y_pred,
-        "shap_values": shap_vals
+        "shap_values": shap_values_list
     }
